636131c2d419aa59609379d62a1173b2901a3480
sanjikumar <sanjikumarsanjikumar0@gmail.com>
2023-12-20 08:46:47 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,30 @@
+document.getElementById("calculate").addEventListener("click", function() {
+    // Get input values
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var operator = document.getElementById("operator").value;
+    var outputType = document.querySelector('input[name="output"]:checked').value;
+    
+    // Perform calculation
+    var result;
+    if (operator === "add") {
+        result = num1 + num2;
+    } else if (operator === "subtract") {
+        result = num1 - num2;
+    } else if (operator === "multiply") {
+        result = num1 * num2;
+    } else if (operator === "divide") {
+        if (num2 === 0) {
+            result = "Error: Division by zero";
+        } else {
+            result = num1 / num2;
+        }
+    }
+
+    // Display result based on outputType
+    if (outputType === "float") {
+        document.getElementById("result").textContent = "Result: " + result.toFixed(2);
+    } else {
+        document.getElementById("result").textContent = "Result: " + Math.round(result);
+    }
+});
